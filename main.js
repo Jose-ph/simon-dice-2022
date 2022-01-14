f6824b28f606f8295e732113b1d59f9703ed1f91
Jose <jose_esteban_basualdo@hotmail.com>
2022-01-14 14:14:03 -0300

    fixes checkPlayerSelection
    The game was always starting the secuence because the selection was true. It needed to check the length of the array AND the selection to execute handle round.

--- main.js
+++ main.js
@@ -4,276 +4,126 @@ const $board = document.querySelector("#board");
 
 const $startButton = document.querySelector("#btn-start");
 
-    let machineColors = [];
-    let playerColors = [];
-    let machineTurn = true;
+let machineColors = [];
+let playerColors = [];
 
+$startButton.onclick = function () {
+  let machineTurn = true;
 
-$startButton.onclick = function (){
+  handleRound(machineColors, playerColors, machineTurn);
+};
 
+function handleRound() {
 
-    handleMachineTurn();
+  lockPlayerClick();
 
-    handlePlayerTurn();
+  handleMachineTurn();
 
-   // setTimeout(() => {
+  highlightColourWithDelay(machineColors);
 
-        
+  let playerDelay = (machineColors.length + 1) * 1000;
 
-       // console.log(compareColors(playerColors,machineColors));
+  setTimeout(() => {
+    console.log("podes jugar!");
 
-       /*  if( !compareColors(playerColors,machineColors)){
-
-            alert("No hay coincidencia, tu pierdes!");
-        }
-
-        else {
-
-            alert("Hay coincidencia, sigue el juego !");
-        } */
-        
-        //   }, 10000);
-    
-
-
-
+    unlockPlayerClick();
+  }, playerDelay);
 }
 
+function unlockPlayerClick(e) {
+  const $colours = document.querySelectorAll(".colour");
 
-function handlePlayerTurn(){
+  let arrayOfColours = Array.from($colours);
 
-    const $colours = document.querySelectorAll('.colour');
-
-    let arrayOfColours = Array.from($colours)
-
-    arrayOfColours.forEach((color) => {
-
-        color.onclick = function(event){
-
-           let newPlayerColor =  getPlayerColor(event);
-
-            console.log(newPlayerColor);
-
-            playerColors.push(newPlayerColor);
-
-            highlightColour(newPlayerColor);
-
-            //comparar colores y luego eliminar el array con la jugada 
-
-            setTimeout(() => {
-
-                let match = compareColors(playerColors,machineColors);
-
-                if(match === true){
-
-                    handleMachineTurn()
-                    handlePlayerTurn();
-                    playerColors = []
-                }
-
-                if ( match === false){
-
-                    alert("perdiste!")
-                    playerColors= []
-                    machineColors = [];
-
-                    alert("toca el bot+on comenzar para volver a jugar")
-                }
-
-                
-               // compareColors(playerColors,machineColors)
-
-              /*   if(compareColors(playerColors,machineColors)){
-
-                    playerColors= []
-                    handleMachineTurn();
-                    handlePlayerTurn();
-
-                }
-
-                if(!compareColors(playerColors,machineColors)){
-
-
-                    alert("perdiste el juego!")
-                    playerColors = []
-                } */
-
-
-               
-                
-            }, 5000);
-           
-        }
-        
-    });
-   
-
-
+  arrayOfColours.forEach((color) => {
+    color.onclick = function (e) {
+      handlePlayerTurn(e);
+    };
+  });
 }
 
+function lockPlayerClick() {
+  const $colours = document.querySelectorAll(".colour");
 
-function handleMachineTurn(){
+  let arrayOfColours = Array.from($colours);
 
-    const $colours = document.querySelectorAll('.colour');
-      
-     let randomColor = getRandomColour($colours);
-
-     //bloquear al jugador
-
-        machineColors.push(randomColor);
-
-        highlightColourWithDelay(machineColors);
-
-        machineTurn= false;
-
-
-
-
+  arrayOfColours.forEach((color) => {
+    color.onclick = function () {
+      console.log("aun no es tu turno");
+    };
+  });
 }
 
+function handlePlayerTurn(e) {
+  let newPlayerColor = e.target;
 
+  playerColors.push(newPlayerColor);
 
-function getPlayerColor(e){
+  highlightColour(newPlayerColor);
+  console.log(playerColors);
 
-    
+  let match = checkPlayerSelection(newPlayerColor);
 
-    let playerColor= e.target
+  if (machineColors.length === playerColors.length && match) {
+    playerColors = [];
+    handleRound();
+  }
 
-    return playerColor;
+  if (match === false) {
+    playerColors = [];
+    machineColors = [];
+    alert("perdiste");
+  }
 }
 
-function getRandomColour(colours){
+function checkPlayerSelection(selection) {
+  let match = true;
 
-    let randomIndex = Math.floor(Math.random()*(4) );
+  for (let i = 0; i < machineColors.length; i++) {
+    if (machineColors[playerColors.length - 1] !== selection) match = false;
+    break;
+  }
 
-    
-
-     return colours[randomIndex]
-
-
-
+  return match;
 }
 
+function handleMachineTurn(machineTurn) {
+  const $colours = document.querySelectorAll(".colour");
 
+  let randomColor = getRandomColour($colours);
 
-function highlightColour (color){
+  machineColors.push(randomColor);
 
-    color.style.opacity = 1;
+  return machineColors;
+}
 
+function getRandomColour(colours) {
+  let randomIndex = Math.floor(Math.random() * 4);
+
+  return colours[randomIndex];
+}
+
+function highlightColour(color) {
+  color.style.opacity = 1;
+
+  setTimeout(() => {
+    color.style.opacity = 0.5;
+  }, 500);
+}
+
+function highlightColourWithDelay(secuence) {
+  let delay = secuence.length * 1000;
+
+  for (let i = 0; i < secuence.length; i++) {
     setTimeout(() => {
-        color.style.opacity = 0.5;
-        
-    }, 500);
+      highlightColour(secuence[i]);
+      console.log("cambia color");
 
+      console.log(delay);
+    }, delay);
 
-    
-
+    delay += 1000;
+  }
 }
 
 
-   
-function highlightColourWithDelay(secuence){
-
-
-    let delay = secuence.length +1 * 1000;
-
-
-    for( let i = 0 ; i<secuence.length; i++){
-
-        setTimeout(() => {
-            highlightColour(secuence[i])
-            console.log("cambia color")
-
-        }, delay);
-        
-        delay+=1000
-
-    }
-
-
-
-}
-   
-function compareColors(playerColors,machineColors){
-
-    let match;
-
-
-    for(let i= 0 ; i < playerColors.length; i++){
-
-        if(playerColors[i].id === machineColors[i].id){
-
-            match = true;
-
-        }
-
-
-        else if(playerColors[i].id !== machineColors[i].id ){
-
-            match = false;
-            break;
-
-        }
-
-
-    }
-
-
-    return match
-
-}
-
-   
-/* function compareColors(userColors,machineColors){
-
-    if(userColors.length != 0){
-
-    for(let i=0 ; i<userColors.length; i++){
-
-
-        if(userColors[i].id === machineColors[i].id){
-
-            return true;
-        }
-
-        else if(userColors[i].id !== machineColors[i].id){
-
-            return false;
-            break;
-        }
-
-        
-       
-    }
-
-    }
-
-    else return 'empty parameter'
-
-} */
-
-/* 
-function compareColors (playerColors,machineColors){
-
-    //la comparación debe ser posición por posicion
-    let matches = false
-
-  machineColors.forEach((color,i) => {
-
-        if( color === playerColors[i]){
-
-            matches = true;
-        }
-        
-      
-    });
-
-
-
-
-    //return match;
-
- 
-
-    return matches;
-} */
